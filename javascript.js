43094c53585b2e9a5e06e322e65b64898f1ce4ba
Ernest <ekim4132@gmail.com>
2022-07-09 14:03:22 -0700

    Implement playRound() in javascript.js

--- javascript.js
+++ javascript.js
@@ -28,8 +28,54 @@ function computerPlay() {
 
 // playRound function:
 // Take two parameters "playerSelection" and "computerSelection"
+function playRound(playerSelection, computerSelection) {
     // Make string value in playerSelection all lowercase
+    playerSelection = playerSelection.toLowerCase();
+    // Define a variable "verdict"
+    let verdict;
     // IF playerSelection is equal to computerSelection
-        // return the string "You tied! playerSelection equals computerSelection"
-    // ELSE IF playerSelection is "rock" and computerSelection
+    if (playerSelection === computerSelection) {
+        // Set verdict to "You tied! playerSelection equals computerSelection"
+        verdict = `You tied! ${playerSelection} ties with ${computerSelection}`;
+    }
+    // ELSE IF playerSelection is "rock"
+    else if (playerSelection === "rock") {
+        // IF computerSelection is "paper"
+        if (computerSelection === "paper") {
+            // Set verdict to "You lose! Paper beats rock"
+            verdict = "You lose! Paper beats rock";
+        }
+        // ELSE
+        else {
+            // Set verdict to "You win! Rock beats scissors"
+            verdict = "You win! Rock beats scissors";
+        }
+    }
+    // ELSE IF playerSelection is "paper"
+    else if (playerSelection === "paper") {
+        // IF computerSelection is "scissors"
+        if (computerSelection === "scissors") {
+            // Set verdict to "You lose! Scissors beats paper"
+            verdict = "You lose! Scissors beats paper";
+        }
+        // ELSE
+        else {
+            // Set verdict to "You win! Paper beats rock"
+            verdict = "You win! Paper beats rock";
+        }
+    }
+    // ELSE
+    else {
+        // IF computerSelection is "rock"
+        if (computerSelection === "rock") {
+            // Set verdict to "You lose! Rock beats scissors"
+            verdict = "You lose! Rock beats scissors";
+        }
+        // ELSE
+        else {
+            // Set verdict to "You win! Scissors beats paper"
+            verdict = "You win! Scissors beats paper";
+        }
+    }
+}
 
